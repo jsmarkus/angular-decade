9c9edbf2bbb517d93f3687e109e9d36b074977e7
Mark <me@jsman.ru>
2014-04-23 09:04:51 +0300

    [change] Added config and UTC

--- demo.js
+++ demo.js
@@ -2,9 +2,6 @@ angular
   .module('demo', ['ngDecade'])
   .controller('DemoCtrl', ['$scope',
     function($scope) {
-      angular.extend($scope, {
-        maxDate: new Date(),
-        minDate: new Date(new Date() - 10 * 365 * 24 * 3600 * 1000)
-      });
+      angular.extend($scope, {});
     }
   ]);
